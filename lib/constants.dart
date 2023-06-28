00685f956bed90cb20880344184f899e4b6eb79e
delfme <53510751+delfme@users.noreply.github.com>
2023-06-28 20:09:52 +0200

    initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+part of flutter_native_splash_cli;
+
+// Web-related constants
+const String _webFolder = 'web/';
+const String _webSplashFolder = '${_webFolder}splash/';
+const String _webSplashImagesFolder = '${_webSplashFolder}img/';
+const String _webIndex = '${_webFolder}index.html';
